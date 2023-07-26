c3e103e5832095d0eb67e25168d67ded3ef35422
João Vitor De Lima <88281625+joaolima16@users.noreply.github.com>
2023-07-26 20:42:08 -0300

    Add files via upload

---
+++ Curso_Udemy/Exercicios/src/Polimorfismo/App.java
@@ -0,0 +1,17 @@
+package Polimorfismo;
+import java.util.ArrayList;
+import java.util.Scanner;
+import Entities.Employee;
+import java.util.List;
+
+public class App {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Employee> lst = new ArrayList<>();
+		System.out.println("write the number of the employees");
+		int n = sc.nextInt();
+		for(int i = 0;i< n;i++) {
+			
+		}
+	}
+}
